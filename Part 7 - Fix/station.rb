212973b55de207d6af76a3d711c041a6da6d2fc7
Vektor|Fenix <avenon7@gmail.com>
2019-08-31 22:20:41 +0300

    Add files via upload
    Исправления согласно замечаниям

---
+++ Part 7 - Fix/station.rb
@@ -0,0 +1,37 @@
+require_relative 'instance_counter'
+require_relative 'validation'
+class Station
+  include InstanceCounter
+  include Validation
+  attr_reader :name, :trains_on_station
+
+  @@all_stations = []
+
+  def initialize(name)
+    @name = name
+    validate!
+    @trains_on_station = []
+    @@all_stations << self
+    register_instance
+  end
+
+  def get_train(train)
+    @trains_on_station << train
+  end
+
+  def type_train(type)
+    @trains.select { |train| train.type == type }.count
+  end
+
+  def train_out(train)
+    @trains_on_station.delete(train)
+  end
+
+  def self.all_stations
+    p @@all_stations
+  end
+
+  def validate!
+    raise 'Название станции не может быть короче 3 символов' if @name.length <= 3
+  end
+end
---
+++ Part 7 - Fix/wagon.rb
@@ -0,0 +1,19 @@
+require_relative 'manufacturer'
+require_relative 'validation'
+
+class Wagon
+  include Manufacturer
+  include Validation
+  attr_reader :type, :number
+
+  def initialize(number)
+    @number = number
+    validate!
+  end
+
+  protected
+
+  def validate!
+    raise 'Ошибка! Правильный формат номера вагона: две любые цифры!' if @number !~ /^[0-9]{2}$/
+  end
+end
